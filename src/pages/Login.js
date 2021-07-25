4418e50fffa4e1ccdba77456dd3833c346bbb7e9
Juliandro Rocha Ribeiro <juliandro.ribeiro@estudante.ifms.edu.br>
2021-07-25 18:13:40 +0000

    First Commit

---
+++ src/pages/Login.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+
+import { Container, Row, Col } from "react-bootstrap";
+
+export default class Login extends Component {
+  render() {
+    return (
+      <Container>
+        <Row>
+          <Col sm={4}></Col>
+          <Col sm={8}></Col>
+        </Row>
+      </Container>
+    );
+  }
+}
